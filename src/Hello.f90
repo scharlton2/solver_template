8a0ad44b82190c9d69a14ea5cb7aa0c45dc18d15
Charlton, Scott R <charlton@usgs.gov>
2021-05-03 11:25:44 -0600

    added boilerplate code

---
+++ src/Hello.f90
@@ -0,0 +1,25 @@
+!  Hello.f90 
+!
+!  FUNCTIONS:
+!  hello - Entry point of console application.
+!
+
+!****************************************************************************
+!
+!  PROGRAM: Hello
+!
+!  PURPOSE:  Entry point for the hello application.
+!
+!****************************************************************************
+
+    program hello
+
+    implicit none
+
+    ! Variables
+
+    ! Body of hello
+    print *, 'Hello World'
+
+    end program hello
+
